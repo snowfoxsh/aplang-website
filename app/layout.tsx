4da5383282bcac9d3eb62dd9e059dd509d85ed59
Patrick Unick <storm1surge@gmail.com>
2025-02-19 17:57:18 -0500

    [uni] add Umami analytics script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import {ThemeProvider} from "next-themes";
 import React from "react";
 import {Toaster} from "@/components/ui/sonner";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,9 +33,16 @@ export default function RootLayout({
   return (
       <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      {/* Umami Analytics Script */}
+      <Script
+          strategy="afterInteractive"
+          src="https://cloud.umami.is/script.js"
+          data-website-id="fb7a7cc7-adf5-4283-89cd-b875fe6dddf1"
+          defer
+      />
       <Toaster position={"bottom-left"} duration={5000}/>
       <ThemeProvider attribute={"class"} defaultTheme={"system"} enableSystem={true} disableTransitionOnChange={true}>
-        {children}
+          {children}
       </ThemeProvider>
       </body>
       </html>
