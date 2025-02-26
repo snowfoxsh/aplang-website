f43112ffeb6147eee82a1f8be25d7bdd792495bf
Patrick Unick <dev_storm@winux.com>
2025-02-25 21:03:00 -0800

    [uni] update MacOS install instructions

--- app/install/macos.tsx
+++ app/install/macos.tsx
@@ -7,22 +7,26 @@ import {Button} from "@/components/ui/button";
 export default function MacOSInstall() {
     return (
         <div>
-            <p>
-                ApLang is available on MacOS with the <CodeLine>.pkg</CodeLine> installer.
-            </p>
+            {/*<p>*/}
+            {/*    ApLang is available on MacOS with the <CodeLine>.pkg</CodeLine> installer.*/}
+            {/*</p>*/}
+            <Link href={"https://github.com/snowfoxsh/aplang/releases/latest/download/aplang.pkg"}>
+                <Button className={""}>
+                    <Download className={"h-4 w-4"}/>
+                    Download Latest
+                </Button>
+            </Link>
             <ol className={"list-decimal pl-6 mt-2 space-y-2"}>
                 <li>
-                    <Link href={"https://github.com/snowfoxsh/aplang/releases/latest/download/aplang.pkg"}>
-                        <Button className={""}>
-                            <Download className={"h-4 w-4"}/>
-                            Download Latest
-                        </Button>
-                    </Link>
-                    Download the latest version from the <Link href="https://github.com/snowfoxsh/aplang/releases/latest" target="_blank" rel="noreferrer"><CodeLine variant={"link"}>Releases</CodeLine></Link> page on the GitHub repository.
-                    You will find the <CodeLine>.pkg</CodeLine> file under <CodeLine>Assets</CodeLine>.
+                    Download the latest version of the ApLang <CodeLine>pkg</CodeLine> Installer.
+                    <br/><br/>
+                    Alternatively, you can find the <CodeLine>pkg</CodeLine> installer on the <Link href="https://github.com/snowfoxsh/aplang/releases" target="_blank"
+                    rel="noreferrer"><CodeLine variant={"link"}>Releases</CodeLine></Link> page on the GitHub
+                    repository.
+                    {/*You will find the <CodeLine>.pkg</CodeLine> file under <CodeLine>Assets</CodeLine>.*/}
                 </li>
                 <li>
-                    Run the installer and follow the instructions to complete the installation. Leave the default installation path as is.
+                Run the installer and follow the instructions to complete the installation. Leave the default installation path as is.
                 </li>
             </ol>
             <Alert className={"border-l-4 border-green-500 mt-4"}>
