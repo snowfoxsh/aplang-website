d957d4be0c509aa60a66347a6988abab24f109e6
Patrick Unick <dev_storm@winux.com>
2025-02-27 13:26:44 -0800

    [uni] update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Link from "next/link";
-import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { ArrowRightIcon, ClipboardCopyIcon } from "@radix-ui/react-icons";
+import {Card, CardHeader, CardContent, CardFooter} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+import {ArrowRightIcon, ClipboardCopyIcon} from "@radix-ui/react-icons";
 import Editor from "@/app/playground/components/editor";
 import Header from "@/app/header";
 import HeartIcon from "@/assets/icons/Heart";
@@ -33,8 +33,17 @@ export default function HomePage() {
     const [sourceCode, setSourceCode] = useState<string>(FizzBuzz);
 
     return (
-        <div className="flex flex-col min-h-screen dark:bg-[#09040b]">
-            <Header />
+        <div className="flex flex-col min-h-screen">
+            <div className={"overflow-x-hidden"}>
+                <Image
+                    src="/gradients/blue-purple-1.svg"
+                    alt="background gradient"
+                    fill
+                    style={{objectPosition: "calc(50% + 25rem) calc(15%)"}}
+                    className="absolute shadow-black/5 inset-0 z-[-1] rotate-180 object-cover pointer-events-none"
+                />
+            </div>
+            <Header/>
             <main className="">
                 <div className="max-w-7xl px-6 mx-auto gap-20 pt-20">
                     <section className="flex flex-col md:flex-row justify-between gap-8">
@@ -48,23 +57,22 @@ export default function HomePage() {
                                 <Button variant="default" asChild>
                                     <Link href="playground">
                                         Try ApLang
-                                        <ArrowRightIcon className="ml-2" />
+                                        <ArrowRightIcon className="ml-2"/>
                                     </Link>
                                 </Button>
-                                <Button variant="outline" className="border-red-800" asChild>
+                                <Button variant="outline" className="border-tertiary" asChild>
                                     <Link href="install">Install ApLang</Link>
                                 </Button>
                             </div>
                             <div className="max-w-lg flex flex-col items-start pt-20">
                                 <div className="text-3xl md:text-4xl flex items-start">
                                     Free &amp; open source
-                                    <HeartIcon className="w-12 h-12 ml-2" />
+                                    <HeartIcon className="w-12 h-12 ml-2"/>
                                 </div>
                                 <div className="pt-2">Aplang is and always will be Free and Open Sourced.</div>
                                 <div className="pt-1">
-                                    This is made possible by our contributors and these companies:
+                                    This is made possible by our contributors and our supporters
                                 </div>
-                                <div className="pt-8">Supported by:</div>
                             </div>
                         </div>
 
@@ -75,7 +83,7 @@ export default function HomePage() {
                                     Your Classic FizzBuzz
                                 </CardHeader>
                                 <CardContent className="p-0 pl-2 pt-1">
-                                    <Editor sourceCode={sourceCode} readonly={true} setSourceCode={setSourceCode} />
+                                    <Editor sourceCode={sourceCode} readonly={true} setSourceCode={setSourceCode}/>
                                 </CardContent>
                                 <CardFooter className="justify-end">
                                     <Button
@@ -83,23 +91,25 @@ export default function HomePage() {
                                         variant="secondary"
                                         onClick={() => navigator.clipboard.writeText(sourceCode)}
                                     >
-                                        <ClipboardCopyIcon className="mr-2 h-4 w-4" /> Copy to clipboard
+                                        <ClipboardCopyIcon className="mr-2 h-4 w-4"/> Copy to clipboard
                                     </Button>
                                 </CardFooter>
                             </Card>
                         </div>
                     </section>
-                    <section className="flex flex-col md:flex-row max-w-md justify-center gap-8 pt-24">
-                        <div className="max-w-lg flex flex-col items-center">
+                    <section className="flex flex-col md:flex-row max-w-md justify-center -translate-x-8 gap-8 pt-10">
+                        <div className="max-w-sm flex flex-col items-center">
+                            <div className="pt-8 w-full text-center text-2xl mb-4">Supporters</div>
                             <Carousel>
                                 <CarouselContent>
                                     <CarouselItem className="basis-1/2">
                                         <div className="p-1">
                                             <Card>
-                                                <CardContent className="flex aspect-square items-center justify-center p-6">
-                                                <span className="text-3xl font-semibold">
-                                                    <AdvancedInstallerLogo className="w-20 h-20"/>
-                                                </span>
+                                                <CardContent
+                                                    className="flex aspect-square items-center justify-center p-4">
+                                                    <span className="text-2xl font-semibold">
+                                                        <AdvancedInstallerLogo className="w-12 h-12"/>
+                                                    </span>
                                                 </CardContent>
                                             </Card>
                                         </div>
@@ -107,20 +117,28 @@ export default function HomePage() {
                                     <CarouselItem className="basis-1/2">
                                         <div className="p-1">
                                             <Card>
-                                                <CardContent className="flex aspect-square items-center justify-center p-6">
-                                                <span className="text-3xl font-semibold">
-                                                    <Image src={SalisBurySchoolLogo} alt="Salisbury School Logo" height={80} />
-                                                </span>
+                                                <CardContent
+                                                    className="flex aspect-square items-center justify-center p-4">
+          <span className="text-2xl font-semibold">
+            <Image
+                src={SalisBurySchoolLogo}
+                alt="Salisbury School Logo"
+                width={43.5}
+                height={48}
+            />
+          </span>
                                                 </CardContent>
                                             </Card>
                                         </div>
                                     </CarouselItem>
                                 </CarouselContent>
-                                <CarouselPrevious className="border-red-500"/>
-                                <CarouselNext className="border-red-500"/>
+
+                                <CarouselPrevious className="border-dashed"/>
+                                <CarouselNext className="border-dashed"/>
                             </Carousel>
                         </div>
                     </section>
+
                 </div>
             </main>
         </div>
