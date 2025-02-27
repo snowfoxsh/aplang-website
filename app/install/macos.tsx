1100d88c47aa356246cc406db8efce31de019d98
Patrick Unick <storm1surge@gmail.com>
2025-02-27 14:01:16 -0800

    Revert "Revert "Website Version 2""

---
+++ app/install/macos.tsx
@@ -0,0 +1,40 @@
+import CodeLine from "@/components/custom/code-line";
+import Link from "next/link";
+import {Download, InfoIcon} from "lucide-react";
+import {Alert} from "@/components/ui/alert";
+import {Button} from "@/components/ui/button";
+
+export default function MacOSInstall() {
+    return (
+        <div className="p-4 space-y-6">
+            <Link href="https://github.com/snowfoxsh/aplang/releases/latest/download/aplang.pkg">
+                <Button className="flex items-center gap-2">
+                    <Download className="h-4 w-4" />
+                    Download Latest
+                </Button>
+            </Link>
+            <ol className="list-decimal pl-6 space-y-4 text-base">
+                <li>
+                    Download the latest version of the ApLang <CodeLine>pkg</CodeLine> Installer.
+                    <br /><br />
+                    Alternatively, you can find the <CodeLine>pkg</CodeLine> installer on the{" "}
+                    <Link href="https://github.com/snowfoxsh/aplang/releases" target="_blank" rel="noreferrer">
+                        <CodeLine variant="link">Releases</CodeLine>
+                    </Link>{" "}
+                    page.
+                </li>
+                <li>
+                    Run the installer and follow the instructions to complete the installation. Leave the default installation path as is.
+                </li>
+            </ol>
+            <Alert className="border-l-4 border-green-500 p-4">
+                <div className="flex items-start">
+                    <InfoIcon className="h-4 w-4 text-green-500 mt-1" />
+                    <div className="ml-2">
+                        Remember to update ApLang regularly to get the latest features and bug fixes.
+                    </div>
+                </div>
+            </Alert>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,146 @@
+"use client";
+
+import React, {useState} from "react";
 import Link from "next/link";
-import {
-    NavigationMenu,
-    NavigationMenuList,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    navigationMenuTriggerStyle
-} from "@/components/ui/navigation-menu";
-import {ColorModeToggle} from "@/app/playground/components/color-mode-toggle";
-import { ArrowRightIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
-import { Button } from "@/components/ui/button";
-import React from "react";
+import {Card, CardHeader, CardContent, CardFooter} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+import {ArrowRightIcon, ClipboardCopyIcon} from "@radix-ui/react-icons";
+import Editor from "@/app/playground/components/editor";
+import Header from "@/app/header";
+import HeartIcon from "@/assets/icons/Heart";
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel";
+import AdvancedInstallerLogo from "@/assets/logos/AdvancedInstallerLogo";
+import Image from "next/image";
+import SalisBurySchoolLogo from "@/assets/logos/SalisburySchoolLogo.png";
+
+const FizzBuzz = `i <- 1
+REPEAT 100 TIMES {
+  s <- ""
+  IF (i MOD 3 == 0) {
+    s <- s + "Fizz"
+  }
+  IF (i MOD 5 == 0) {
+    s <- s + "Buzz"
+  }
+  IF (LENGTH(s) == 0) {
+    s <- i
+  }
+  DISPLAY(s)
+  i <- i + 1
+}`;
 
 export default function HomePage() {
+    const [sourceCode, setSourceCode] = useState<string>(FizzBuzz);
+
     return (
         <div className="flex flex-col min-h-screen">
-            <header className="z-[50] sticky top-0 w-full bg-background/95 border-b backdrop-blur-sm dark:bg-black/[0.6] border-border/40">
-                <div className="container h-14 flex items-center">
-                    <Link href="/" className="flex justify-start items-center hover:opacity-85 transition-opacity duration-300 mr-4">
-                        <span className="font-bold text-lg mt-[-0.075rem]">ApLang</span>
-                        <span className="sr-only">ApLang</span>
-                    </Link>
-                    <NavigationMenu>
-                        <NavigationMenuList>
-                            <NavigationMenuItem>
-                                <Link href="/book" passHref legacyBehavior>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                        <span className="font-regular text-base">Book</span>
-                                        <span className="sr-only">Book</span>
-                                    </NavigationMenuLink>
-                                </Link>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <Link href="/playground" passHref legacyBehavior>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                        <span className="font-regular text-base">Playground</span>
-                                        <span className="sr-only">Playground</span>
-                                    </NavigationMenuLink>
-                                </Link>
-                            </NavigationMenuItem>
-                        </NavigationMenuList>
-                    </NavigationMenu>
-                    <NavigationMenu className="ml-auto flex items-center gap-2">
-                        <NavigationMenuList>
-                            <NavigationMenuItem>
-                                <Link href="https://github.com/snowfoxsh/aplang" passHref legacyBehavior>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                        <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]"/>
-                                    </NavigationMenuLink>
-                                </Link>
-                            </NavigationMenuItem>
-                        <ColorModeToggle/>
-                        </NavigationMenuList>
-                    </NavigationMenu>
-                </div>
-            </header>
-            <main className="min=h-[calc(100vh-57px)] flex-1">
-                <div className="container relative pb-10">
-                    <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-21 lb:pb-6">
-                        <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
-                            ApLang
-                        </h1>
-                        <span className="max-w-[800px] text-center text-lg font-light text-foreground">
-                            A programming language made to aid students who are taking AP Computer Science Principals
-                        </span>
-                        <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
-                            <Button variant="default" asChild>
-                                <Link href="/playground">
-                                    Playground
-                                    <ArrowRightIcon className="ml-2" />
-                                </Link>
-                            </Button>
-                            <Button variant="outline" asChild>
-                                <Link href="/book" target="_blank" rel="noopener noreferrer">Learn ApLang</Link>
-                            </Button>
+            <div className={"overflow-x-hidden"}>
+                <Image
+                    src="/gradients/blue-purple-1.svg"
+                    alt="background gradient"
+                    fill
+                    style={{objectPosition: "calc(50% + 25rem) calc(15%)"}}
+                    className="absolute shadow-black/5 inset-0 z-[-1] rotate-180 object-cover pointer-events-none"
+                />
+            </div>
+            <Header/>
+            <main className="">
+                <div className="max-w-7xl px-6 mx-auto gap-20 pt-20">
+                    <section className="flex flex-col md:flex-row justify-between gap-8">
+                        {/* Left column (title, text, CTA) */}
+                        <div className="max-w-md space-y-4">
+                            <h1 className="text-4xl font-bold tracking-tight md:text-5xl">Aplang</h1>
+                            <p className="text-lg">
+                                A programming language made to aid students taking AP Computer Science Principles.
+                            </p>
+                            <div className="flex items-center space-x-4 pt-4">
+                                <Button variant="default" asChild>
+                                    <Link href="playground">
+                                        Try ApLang
+                                        <ArrowRightIcon className="ml-2"/>
+                                    </Link>
+                                </Button>
+                                <Button variant="outline" className="border-tertiary" asChild>
+                                    <Link href="install">Install ApLang</Link>
+                                </Button>
+                            </div>
+                            <div className="max-w-lg flex flex-col items-start pt-20">
+                                <div className="text-3xl md:text-4xl flex items-start">
+                                    Free &amp; open source
+                                    <HeartIcon className="w-12 h-12 ml-2"/>
+                                </div>
+                                <div className="pt-2">Aplang is and always will be Free and Open Sourced.</div>
+                                <div className="pt-1">
+                                    This is made possible by our contributors and our supporters
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Right column (code editor in a card) */}
+                        <div className="flex w-full md:w-1/2 items-center">
+                            <Card className="relative w-full">
+                                <CardHeader className="flex justify-center p-4 border-b">
+                                    Your Classic FizzBuzz
+                                </CardHeader>
+                                <CardContent className="p-0 pl-2 pt-1">
+                                    <Editor sourceCode={sourceCode} readonly={true} setSourceCode={setSourceCode}/>
+                                </CardContent>
+                                <CardFooter className="justify-end">
+                                    <Button
+                                        className="absolute bottom-7 right-7"
+                                        variant="secondary"
+                                        onClick={() => navigator.clipboard.writeText(sourceCode)}
+                                    >
+                                        <ClipboardCopyIcon className="mr-2 h-4 w-4"/> Copy
+                                    </Button>
+                                </CardFooter>
+                            </Card>
                         </div>
                     </section>
+                    <section className="flex flex-col md:flex-row max-w-md justify-center -translate-x-8 gap-8 pt-10">
+                        <div className="max-w-sm flex flex-col items-center">
+                            <div className="pt-8 w-full text-center text-2xl mb-4">Supporters</div>
+                            <Carousel>
+                                <CarouselContent>
+                                    <CarouselItem className="basis-1/2">
+                                        <div className="p-1">
+                                            <Card>
+                                                <CardContent
+                                                    className="flex aspect-square items-center justify-center p-4">
+                                                    <span className="text-2xl font-semibold">
+                                                        <AdvancedInstallerLogo className="w-12 h-12"/>
+                                                    </span>
+                                                </CardContent>
+                                            </Card>
+                                        </div>
+                                    </CarouselItem>
+                                    <CarouselItem className="basis-1/2">
+                                        <div className="p-1">
+                                            <Card>
+                                                <CardContent
+                                                    className="flex aspect-square items-center justify-center p-4">
+          <span className="text-2xl font-semibold">
+            <Image
+                src={SalisBurySchoolLogo}
+                alt="Salisbury School Logo"
+                width={43.5}
+                height={48}
+            />
+          </span>
+                                                </CardContent>
+                                            </Card>
+                                        </div>
+                                    </CarouselItem>
+                                </CarouselContent>
+
+                                <CarouselPrevious className="border-dashed"/>
+                                <CarouselNext className="border-dashed"/>
+                            </Carousel>
+                        </div>
+                    </section>
+
                 </div>
             </main>
         </div>
-    )
+    );
 }
